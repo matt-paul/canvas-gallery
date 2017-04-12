51ef39820524d85c504a00d34659c2f4db55f883
Matt Paul <mattpaul001@gmail.com>
2017-04-12 12:18:04 +0100

    Sketched out api endpoints that are required

--- api/server.js
+++ api/server.js
@@ -1,10 +1,25 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
-app.get('/', function (req, res) {
+const pictures = [];
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(8000, function () {
+app.get('/pictures', (req, res) => {
+  res.send('This is going to return a list of pictures');
+})
+
+app.get('/picture/picture-here', (req, res) => {
+  res.send('endpoint for each picture');
+})
+
+app.post('/new', (req, res) => {
+  res.send('This is going to post a picture object')
+  // Instantiate the picture class, and then push to the pictures array
+})
+
+app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
 })
