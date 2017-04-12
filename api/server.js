fbff4d7534eea319633330878544cfaf10de84e8
Matt Paul <mattpaul001@gmail.com>
2017-04-12 20:41:23 +0100

    Installed uuid to uniquely identify pictures, each picture now has unique url

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const uuidV1 = require('uuid/v1');
 
 class Picture {
   constructor(name, width = 100, height = 100) {
     this.name = name;
     this.height = height;
     this.width = width;
+    this.uuid = uuidV1();
   }
 }
 const pictures = [];
@@ -22,23 +24,27 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.get('/pictures', (req, res) => {
   res.send(pictures);
-})
+});
 
-app.get('/picture/picture-here', (req, res) => {
-  res.send('endpoint for each picture');
-})
+app.get('/picture/:uuid', (req, res) => {
+  const uuid = req.params.uuid;
+  const pic = pictures.filter((picture) => {
+    return picture.uuid === uuid;
+  });
+  res.send(pic);
+});
 
 app.post('/new', (req, res) => {
   const { name, width, height } = req.body;
   const p = new Picture(name, width, height);
   pictures.push(p);
-  res.status(201).location('/pictures/' + p.height).json(p);
-})
+  res.status(201).location('/pictures/' + p.uid).json(p);
+});
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-})
+  console.log('Example app listening on port 8000!');
+});
