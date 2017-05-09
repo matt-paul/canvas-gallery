e715530108dc03d9cc53a473e250ac87f89d4d0c
Matt Paul <mattpaul001@gmail.com>
2017-05-09 12:30:52 +0100

    User can draw picture and select colours from the colour picker

--- frontend/src/components/Picture.js
+++ frontend/src/components/Picture.js
@@ -5,10 +5,10 @@ import styled from 'styled-components';
 class Picture extends React.Component {
   constructor(props) {
     super(props);
-    // this.updateCanvas = this.updateCanvas.bind(this);
+    this.updateCanvas = this.updateCanvas.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
     this.onMouseMove = this.onMouseMove.bind(this);
     this.onMouseUp = this.onMouseUp.bind(this);
-    this.onMouseDown = this.onMouseDown.bind(this);
     this.mouseX = '';
     this.mouseY = '';
     this.ctx = '';
@@ -16,26 +16,23 @@ class Picture extends React.Component {
   }
 
   componentDidMount() {
-    this.ctx = this.refs.canvas.getContext('2d');
-    // this.updateCanvas();
+    this.ctx = this.canvas.getContext('2d');
+    this.updateCanvas();
   }
 
-  // updateCanvas() {
-  //   console.log(this.ctx)
-  //   this.rect = this.ctx.getBoundingClientRect(); // this is used to counter the offset, ie the circle now appears with its center exactly at mouse position.
-  // }
-  //
   onMouseDown(event) {
+    console.log('mouse is down, whataya gonna do?')
+    console.log(this.rect)
     this.mouseX = event.clientX - this.rect.left;
     this.mouseY = event.clientY - this.rect.top;
-    // canvas.addEventListener('mousemove', this.onMouseMove);
-    // document.body.addEventListener('mouseup', this.onMouseUp); //this is on the document body in case the mouse up event happens off the canvas!
-
+    this.canvas.addEventListener('mousemove', this.onMouseMove);
+    document.body.addEventListener('mouseup', this.onMouseUp);
   }
+
   onMouseMove(event) {
     this.ctx.beginPath();
-    this.ctx.strokeStyle = this.refs.colorPicker.value;
-    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = this.colorPicker.value;
+    this.ctx.lineWidth = 10;
     this.ctx.moveTo(this.mouseX, this.mouseY); // Start at the mouse position where mouse started to move
     this.mouseX = event.clientX - this.rect.left;
     this.mouseY = event.clientY - this.rect.top;
@@ -44,38 +41,31 @@ class Picture extends React.Component {
   }
 
   onMouseUp(event) {
-    this.ctx.removeEventListener('mousemove', this.onMouseMove);
+    this.canvas.removeEventListener('mousemove', this.onMouseMove);
     document.body.removeEventListener('mouseup', this.onMouseUp)
   }
 
+  updateCanvas() {
+    this.rect = this.canvas.getBoundingClientRect(); // this is used to counter the offset, ie the circle now appears with its center exactly at mouse position.
+  }
   render() {
     const url = `picture/${this.props.uuid}`;
 
     return (
-      <div>
-        <a href={url}>
+        <li>
           <canvas
-            ref="canvas"
-            className="picture"
+            onMouseDown={this.onMouseDown}
+            ref={(canvas) => { this.canvas = canvas; }}
+            className={this.props.className}
             width={this.props.width}
             height={this.props.height}/>
-          <input ref="colorpicker" type="color"/>
-        </a>
-
-      </div>
+          <input
+            ref={(colorPicker) => { this.colorPicker = colorPicker; }}
+            type="color"/>
+      </li>
     );
   }
-
 }
-
-
-export default styled(Picture)`
-  display: inline-block;
-  border: 1px solid green;
-  background-color: black;
-  height: 1000px;
-`
-;
 
 Picture.propTypes = {
   name: PropTypes.string.isRequired,
@@ -83,18 +73,15 @@ Picture.propTypes = {
   width: PropTypes.string.isRequired,
   uuid: PropTypes.string.isRequired,
 }
-// window.onload = () => {
-//   const canvas = document.querySelector('#canvas');
-//   const this.ctx = canvas.getthis.ctx('2d');
-//
-//   let colorPicker = document.querySelector('#colorpicker');
-//   let color;
-//
-//   colorPicker.addEventListener('onchange', () => {
-//     color = colorPicker.value;
-//   })
-//
-//
-//   canvas.addEventListener('mousedown', onMouseDown)
-//
-// }
+
+export default styled(Picture)`
+  box-style: border-box;
+  background-color:#ddc;
+  border:solid 20px #eee;
+  border-bottom-color:#fff;
+  border-left-color:#eee;
+  border-radius:2px;
+  border-right-color:#eee;
+  border-top-color:#ddd;
+  box-shadow:0 0 5px 0 rgba(0,0,0,.25) inset, 0 5px 10px 5px rgba(0,0,0,.25);
+`;
